5b8896d35cad15a61bdfe29c94213844185e6052
Masoud <masoud.rad.uk@gmail.com>
2023-08-10 19:46:38 +0100

    TDD is installed and it is working properly

--- __tests__/utils.test.ts
+++ __tests__/utils.test.ts
@@ -1,8 +1,8 @@
-const {formatComments} = require ('./db/seed/utils.ts')
+const {formatComments} = require ('../db/seeds/utils.ts')
 
 interface LandTemple {
     land_id: number;
-    landName: string;
+    landname: string;
     city: string;
     country: string;
     description: string;
@@ -18,13 +18,20 @@ interface CommentTemple {
     created_at: Date;
 }
 
+type resultCommentTemple {
+    body: string;
+    land_id: number;
+    username: string;
+    created_at: Date;
+}
+
 
 describe("formatComments", ()=>{
     test("if the function takes expty array should return empty array",()=>{
         const comments : CommentTemple[] = [];
         const lands : LandTemple[] = [{
             land_id: 1,
-            landName: 'Media City Salford Quays',
+            landname: 'Media City Salford Quays',
             city: 'Salford',
             country: 'England',
             description: 'popular place for skating on the weekends! Media City Salford Quays is set in the Salford district of Manchester, 400 metres from The Lowry and 4.3 km from Opera House Manchester.',
@@ -35,7 +42,7 @@ describe("formatComments", ()=>{
         },
         {
             land_id: 2,
-            landName: 'Heaton Park',
+            landname: 'Heaton Park',
             city: 'Manchester',
             country: 'England',
             description: 'Urban park with a championship golf course for grown-ups, farm animals and play areas for kids.',
@@ -45,5 +52,61 @@ describe("formatComments", ()=>{
             username: 'cooljmessy'
         }];
         expect(formatComments(comments, lands)).toEqual([])
+    })
+
+    test("if the function takes expty array should return empty array",()=>{
+        const comments : CommentTemple[] = [];
+        const lands : LandTemple[] = [];
+        expect(formatComments(comments, lands)).toEqual([])
+    })
+
+    test("the function should return a new comments array which landName is replaced with land_id",()=>{
+        const comments : CommentTemple[] = [{
+            body: "Itaque quisquam est similique et est perspiciatis reprehenderit voluptatem autem. Voluptatem accusantium eius error adipisci quibusdam doloribus.",
+            username: "tickle122",
+            landName: 'Media City Salford Quays',
+            created_at: new Date("2023-08-10T12:00:00Z")
+          },
+          {
+            body: "Great park! We visited in June. We saw the animals, grabbed a drink and an ice cream which I felt we're reasonably priced. We took a picnic. Went down to the lake, played ball on the field. Great place to spend an afternoon. Easy access via Metrolink from where we live",
+            username: "grumpy19",
+            landName: 'Heaton Park',
+            created_at: new Date("2023-08-10T12:00:00Z")
+          }];
+        const lands : LandTemple[] = [{
+            land_id: 1,
+            landname: 'Media City Salford Quays',
+            city: 'Salford',
+            country: 'England',
+            description: 'popular place for skating on the weekends! Media City Salford Quays is set in the Salford district of Manchester, 400 metres from The Lowry and 4.3 km from Opera House Manchester.',
+            created_at: new Date("2023-08-10T12:00:00Z"),
+            vote: 8,
+            land_img_url: 'https://thedeveloper.live/AcuCustom/Sitename/DAM/130/MediaCityUKlead.jpg',
+            username: 'cooljmessy'
+        },
+        {
+            land_id: 2,
+            landname: 'Heaton Park',
+            city: 'Manchester',
+            country: 'England',
+            description: 'Urban park with a championship golf course for grown-ups, farm animals and play areas for kids.',
+            created_at: new Date("2023-08-10T12:00:00Z"),
+            vote: 8,
+            land_img_url: 'https://thedeveloper.live/AcuCustom/Sitename/DAM/130/MediaCityUKlead.jpg',
+            username: 'cooljmessy'
+        }];
+        const result : resultCommentTemple[] =[{
+            body: "Itaque quisquam est similique et est perspiciatis reprehenderit voluptatem autem. Voluptatem accusantium eius error adipisci quibusdam doloribus.",
+            username: "tickle122",
+            land_id: 1,
+            created_at: new Date("2023-08-10T12:00:00Z")
+          },
+          {
+            body: "Great park! We visited in June. We saw the animals, grabbed a drink and an ice cream which I felt we're reasonably priced. We took a picnic. Went down to the lake, played ball on the field. Great place to spend an afternoon. Easy access via Metrolink from where we live",
+            username: "grumpy19",
+            land_id: 2,
+            created_at: new Date("2023-08-10T12:00:00Z")
+          }]
+        expect(formatComments(comments, lands)).toEqual(result);
     })
 })
---
+++ api.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("hey skaters");
+//# sourceMappingURL=api.js.map
